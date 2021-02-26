eee726be8163191e2f5f59b6304cf872f2ce5cd8
enrique gonzalez isarrualde <enriquegonzalezisarrualde@gmail.com>
2021-02-26 17:00:46 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio 2 remplazar numeros-nombres/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace Ejercicio_2_remplazar_numeros_nombres
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.txMensaje = new System.Windows.Forms.TextBox();
+            this.labeMensaje = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(306, 148);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(116, 37);
+            this.btnConvertir.TabIndex = 0;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // txMensaje
+            // 
+            this.txMensaje.Location = new System.Drawing.Point(306, 102);
+            this.txMensaje.Name = "txMensaje";
+            this.txMensaje.Size = new System.Drawing.Size(100, 26);
+            this.txMensaje.TabIndex = 1;
+            // 
+            // labeMensaje
+            // 
+            this.labeMensaje.AutoSize = true;
+            this.labeMensaje.Location = new System.Drawing.Point(302, 215);
+            this.labeMensaje.Name = "labeMensaje";
+            this.labeMensaje.Size = new System.Drawing.Size(51, 20);
+            this.labeMensaje.TabIndex = 2;
+            this.labeMensaje.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labeMensaje);
+            this.Controls.Add(this.txMensaje);
+            this.Controls.Add(this.btnConvertir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.TextBox txMensaje;
+        private System.Windows.Forms.Label labeMensaje;
+    }
+}
+
---
+++ Ejercicio 2 remplazar numeros-nombres/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ Escribe una frase e incluye muchos caracteres numéricos (0-9), pero decide que para los números 10 y menos prefiere escribir la palabra. ¿Puedes ingresar y editar tu frase para escribir el nombre de cada número en lugar de usar el número?
+No
+Tarea:
+Tome una frase y reemplace cualquier instancia de un número entero del 0 al 10 y reemplácela con la palabra en inglés que corresponda a ese número entero.
+
+Formato de entrada:
+Una cadena de la frase en su forma original (minúscula).
+
+Formato de salida:
+Una cadena de la frase actualizada que ha cambiado los números a palabras.
+
+Entrada de muestra:
+necesito 2 calabazas y 3 manzanas
+
+Salida de muestra:
+necesito dos calabazas y tres manzanas
+
+ 
+ */
+namespace Ejercicio_2_remplazar_numeros_nombres
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            string frase = txMensaje.Text;
+            string fraseNueva="";
+            for(int i =0; i< frase.Length; i++)
+            {
+              char caracter= frase.ElementAt(i);
+                switch (caracter)
+                {
+                    case '1':
+                        fraseNueva = fraseNueva + " uno ";
+                        break;
+                    case '2':
+                        fraseNueva = fraseNueva + " dos ";
+                        break;
+                    case '3':
+                        fraseNueva = fraseNueva + " tres ";
+                        break;
+                    case '4':
+                        fraseNueva = fraseNueva + " cuatro ";
+                        break;
+                    case '5':
+                        fraseNueva = fraseNueva + " cinco ";
+                        break;
+                    case '6':
+                        fraseNueva = fraseNueva + " seis ";
+                        break;
+                    case '7':
+                        fraseNueva = fraseNueva + " siete ";
+                        break;
+                    case '8':
+                        fraseNueva = fraseNueva + " ocho ";
+                        break;
+                    case '9':
+                        fraseNueva = fraseNueva + " nueve ";
+                        break;
+                  
+
+                    case ' ':
+                        fraseNueva = fraseNueva + " ";
+                        break;
+
+                    default:
+                        fraseNueva = fraseNueva + caracter;
+                        break;
+                }
+                
+
+
+
+            }
+
+            labeMensaje.Text = fraseNueva;
+        }
+    }
+}
